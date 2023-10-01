c43d37a586f82568d57c307e6d4621af8d626aee
thainara2410 <thaynarar518@gmail.com>
2023-10-01 16:00:33 -0300

    First upload
    Inserção das classes A e B

---
+++ A.java
@@ -0,0 +1,26 @@
+public class A{
+    Integer a1;
+    Float a2;
+
+
+    public Integer getA1() {
+        return a1;
+    }
+    public void setA1(Integer a1) {
+        this.a1 = a1;
+    }
+    public Float getA2() {
+        return a2;
+    }
+    public void setA2(Float a2) {
+        this.a2 = a2;
+    }
+
+    public void ma1(){
+        System.out.println("a2");
+    }
+
+    public void ma2(){
+        System.out.println("a2");
+    }
+}
---
+++ B.java
@@ -0,0 +1,25 @@
+public class B{
+    Integer b1;
+    Float b2;
+
+
+    public Integer getB1() {
+        return b1;
+    }
+    public void setB1(Integer b1) {
+        this.b1 = b1;
+    }
+    public Float getB2() {
+        return b2;
+    }
+    public void setB2(Float b2) {
+        this.b2 = b2;
+    }
+    public void mb1(){
+        System.out.println("b1");
+    }
+
+    public void mb2(){
+        System.out.println("b2");
+    }
+}
