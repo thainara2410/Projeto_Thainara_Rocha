6ccbc181e286941dfe98596545793ca1066d62e3
Alexandra Melo <100172257+alexandra-melo@users.noreply.github.com>
2023-10-03 20:09:39 -0300

    Alteracao classe A
    Metodo getSoma()

--- A.java
+++ A.java
@@ -1,30 +1,37 @@
-public class A{
+public class A {
     Integer a1;
     Float a2;
-
 
     public Integer getA1() {
         return a1;
     }
+
     public void setA1(Integer a1) {
         this.a1 = a1;
     }
+
     public Float getA2() {
         return a2;
     }
+
     public void setA2(Float a2) {
         this.a2 = a2;
     }
 
-    public void ma1(){
+    public void ma1() {
         System.out.println("a2");
     }
 
-    public void ma2(){
+    public void ma2() {
         System.out.println("a2");
     }
 
-    public void ma3(){
+    public void ma3() {
         System.out.println("Alteração a classe A partir do clone");
     }
+
+    public int getSoma(int a, int b) {
+        return a + b;
+    }
+
 }
