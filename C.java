7d20bdd58b429dca20541d34ca19378a09231590
thainara2410 <thaynarar518@gmail.com>
2023-10-03 10:32:50 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,26 @@
+public class C {
+    String c1;
+    Integer c2;
+
+
+    public String getC1() {
+        return c1;
+    }
+    public void setC1(String c1) {
+        this.c1 = c1;
+    }
+    public Integer getC2() {
+        return c2;
+    }
+    public void setC2(Integer c2) {
+        this.c2 = c2;
+    }
+
+    public void mc1(){
+        System.out.println("c2");
+    }
+
+    public void mc2(){
+        System.out.println("c2");
+    }
+}
